55a7078e686e4f56a5d3e755e73ee804feb0f6e8
miraclecommerce <foxshopify0127@gmail.com>
2022-09-26 15:59:21 -0400

    Upgrade theme 2.0

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=7a09c3c9-d175-4b28-8d29-af6c481976ae';document.getElementsByTagName('head')[0].appendChild(script);
